1f53ac56ae41561975a75138924fc4f7c0998587
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2024-10-31 16:19:58 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv"
+dotenv.config();
+
+import express from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import connectDB from "./utils/db.js";
+
+import { app, server } from "./socket/socket.js"
+
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}
+app.use(cors(corsOptions));
+
+
+// api route
+import userRoute from "./Routes/user.route.js"
+import postRoute from "./Routes/post.route.js"
+import messageRoute from "./Routes/message.route.js"
+
+// yha pr apni api ayengi
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoute);
+app.use("/api/v1/message", messageRoute);
+
+
+// server Running 
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is Running On POST: ${PORT}`);
+})
---
+++ server/socket/socket.js
@@ -0,0 +1,36 @@
+import { Server } from "socket.io"
+import express from 'express'
+import http from "http"
+
+const app = express();
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+})
+
+const userSocketMap = {} // this maps socket it to User._id,
+
+const getReceiverSocketId = (receiverId) => userSocketMap[receiverId]
+
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    if (userId) {
+        userSocketMap[userId] = socket.id;
+        console.log(`user Connected: UserId: ${userId}, socketId: ${socket.id}`);
+    }
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+
+
+    socket.on("disconnect", () => {
+        if (userId) {
+            delete userSocketMap[userId];
+        }
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+    })
+})
+export { app, server, io, getReceiverSocketId }
