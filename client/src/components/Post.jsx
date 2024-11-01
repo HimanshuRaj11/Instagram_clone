d33971480dc104682eab09b202171539662b8ea4
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2024-11-01 13:22:59 +0530

    fix urls of server

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -98,7 +98,7 @@ const Post = ({ post }) => {
 
     const bookmarkHandler = async () => {
         try {
-            const res = await axios.get(`http://localhost:8000/api/v1/post/${post?._id}/bookmark`, { withCredentials: true });
+            const res = await axios.get(`${import.meta.env.VITE_SERVER_URL}api/v1/post/${post?._id}/bookmark`, { withCredentials: true });
             if (res.data.success) {
                 toast.success(res.data.message);
             }
