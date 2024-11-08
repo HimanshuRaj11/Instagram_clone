be239637e26e8153e6dd50448c0be512c7dc8d2a
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2024-11-08 11:53:22 +0530

    fixed  server error

--- server/server.js
+++ server/server.js
@@ -15,7 +15,10 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
     origin: process.env.CLIENT_URL,
-    credentials: true
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
+    credentials: true,
+    preflightContinue: false,
 }
 app.use(cors(corsOptions));
 
